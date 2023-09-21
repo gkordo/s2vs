add7572664b4896cb01eb13aaee5cf57d044e90f
gkordo <georgekordopatis@iti.gr>
2023-09-21 20:30:15 +0200

    Add hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,16 @@
+from model.similarity_network import SimilarityNetwork
+from model.feature_extractor import FeatureExtractor
+
+dependencies = ["torch", "torchvision", "einops"]
+
+
+def resnet50_extractor(dims=512):
+    return FeatureExtractor['RESNET'].get_model(dims)
+
+
+def s2vs_dns():
+    return SimilarityNetwork['ViSiL'].get_model(pretrained='s2vs_dns')
+
+
+def s2vs_vcdb():
+    return SimilarityNetwork['ViSiL'].get_model(pretrained='s2vs_vcdb')
